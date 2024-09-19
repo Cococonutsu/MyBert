d5a7044831b8d7797a99efd25f9b7211ebfe629f
suchen <872115603@qq.com>
2024-09-19 23:33:17 +0800

    2024/9/19——修改Transformer多头注意力实现方式

--- Bert_encoder.py
+++ Bert_encoder.py
@@ -37,14 +37,24 @@ class Multi_Head_Attention(nn.Module):
         self.K = nn.Linear(hidden_size, hidden_size)
         self.V = nn.Linear(hidden_size, hidden_size)
 
+    # def forward(self, x):  # batch, seq_len, hidden_size
+    #     batch, seq_len, hidden_size = x.shape  # batch, seq_len, hidden_size
+    #     x = x.reshape(batch, self.head_num, -1, hidden_size)  # batch, head_num, (seq_len//head_num), hidden_size
+    #     q = self.Q(x)  # batch, head_num, (seq_len//head_num), hidden_size
+    #     k = torch.transpose(self.K(x), -1, -2)  # batch, head_num, (seq_len//head_num), hidden_size
+    #     v = self.V(x)  # batch, head_num, (seq_len//head_num), hidden_size
+    #     att = self.softmax((q @ k) / hidden_size) @ v
+    #     att = att.reshape(batch, seq_len, hidden_size)
+    #     return att#([40, 128, 768])
+
     def forward(self, x):  # batch, seq_len, hidden_size
         batch, seq_len, hidden_size = x.shape  # batch, seq_len, hidden_size
-        x = x.reshape(batch, self.head_num, -1, hidden_size)  # batch, head_num, (seq_len//head_num), hidden_size
-        q = self.Q(x)  # batch, head_num, (seq_len//head_num), hidden_size
-        k = torch.transpose(self.K(x), -1, -2)  # batch, head_num, (seq_len//head_num), hidden_size
-        v = self.V(x)  # batch, head_num, (seq_len//head_num), hidden_size
-        att = self.softmax((q @ k) / hidden_size) @ v
-        att = att.reshape(batch, seq_len, hidden_size)
+        q, k, v = self.Q(x), self.K(x), self.V(x)
+        q = q.reshape(batch, seq_len, self.head_num, -1).transpose(1, 2)
+        k = k.reshape(batch, seq_len, self.head_num, -1).transpose(1, 2)
+        v = v.reshape(batch, seq_len, self.head_num, -1).transpose(1, 2)
+        att = self.softmax((q @ k.transpose(-1, -2)) / torch.sqrt(torch.tensor(q.size(-1), dtype=torch.float32))) @ v
+        att = att.transpose(1, 2).reshape(batch, seq_len, hidden_size)
         return att
 
 
@@ -70,13 +80,13 @@ class Cls_Attention(nn.Module):
     def __init__(self, hidden_size, head_num):
         super().__init__()
         self.head_num = head_num
-        self.linear = nn.Linear(hidden_size//head_num, hidden_size//head_num)
+        self.linear = nn.Linear(hidden_size // head_num, hidden_size // head_num)
         self.relu = nn.ReLU()
 
-    def forward(self, x):# batch_size hiddem_size
+    def forward(self, x):  # batch_size hiddem_size
         batch_size, hidden_size = x.shape
-        x = x.reshape(batch_size, self.head_num, -1)# batch_size head_num (hiddem_size//head_num)
-        x = self.linear(x)# batch_size head_num (hiddem_size//head_num)
-        x = self.relu(x)# batch_size head_num (hiddem_size//head_num)
+        x = x.reshape(batch_size, self.head_num, -1)  # batch_size head_num (hiddem_size//head_num)
+        x = self.linear(x)  # batch_size head_num (hiddem_size//head_num)
+        x = self.relu(x)  # batch_size head_num (hiddem_size//head_num)
         x = x.reshape(batch_size, hidden_size)
         return x
